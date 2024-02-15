2f7262b8d06d5c0de87369606c14c0901e357962
Mathiew82 <amateo1982@yahoo.es>
2024-02-15 21:35:25 +0100

    Replaced nerd font icons to unicode symbols

--- neo.py
+++ neo.py
@@ -18,78 +18,96 @@ selection = input("")
 
 if selection == "1":
     system("git clone https://github.com/Mathiew82/lazyvim-config.git ~/.config/nvim")
-    print(" Installation was successful!")
+    print("\N{heavy check mark} Installation was successful!")
 elif selection == "2":
     try:
         if system("sudo rm -r ~/.config/nvim") != 0:
-            raise Exception("󱂷 ~/.config/nvim folder does not exist")
-        print("󱥾 Deleted ~/.config/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.config/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.config/nvim folder...")
     except Exception as e:
         print(e)
 
     system("git clone https://github.com/Mathiew82/lazyvim-config.git ~/.config/nvim")
-    print(" Update was successful!")
+    print("\N{heavy check mark} Update was successful!")
 elif selection == "3":
     try:
         if system("sudo rm -r ~/.config/nvim") != 0:
-            raise Exception("󱂷 ~/.config/nvim folder does not exist")
-        print("󱥾 Deleted ~/.config/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.config/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.config/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.local/share/nvim") != 0:
-            raise Exception("󱂷 ~/.local/share/nvim folder does not exist")
-        print("󱥾 Deleted ~/.local/share/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.local/share/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.local/share/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.local/state/nvim") != 0:
-            raise Exception("󱂷 ~/.local/state/nvim folder does not exist")
-        print("󱥾 Deleted ~/.local/state/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.local/state/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.local/state/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.cache/nvim") != 0:
-            raise Exception("󱂷 ~/.cache/nvim folder does not exist")
-        print("󱥾 Deleted ~/.cache/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.cache/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.cache/nvim folder...")
     except Exception as e:
         print(e)
 
-    print(" Uninstall was successful!")
+    print("\N{heavy check mark} Uninstall was successful!")
 elif selection == "4":
     try:
         if system("sudo rm -r ~/.config/nvim") != 0:
-            raise Exception("󱂷 ~/.config/nvim folder does not exist")
-        print("󱥾 Deleted ~/.config/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.config/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.config/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.local/share/nvim") != 0:
-            raise Exception("󱂷 ~/.local/share/nvim folder does not exist")
-        print("󱥾 Deleted ~/.local/share/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.local/share/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.local/share/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.local/state/nvim") != 0:
-            raise Exception("󱂷 ~/.local/state/nvim folder does not exist")
-        print("󱥾 Deleted ~/.local/state/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.local/state/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.local/state/nvim folder...")
     except Exception as e:
         print(e)
 
     try:
         if system("sudo rm -r ~/.cache/nvim") != 0:
-            raise Exception("󱂷 ~/.cache/nvim folder does not exist")
-        print("󱥾 Deleted ~/.cache/nvim folder...")
+            raise Exception(
+                "\N{heavy multiplication x} ~/.cache/nvim folder does not exist"
+            )
+        print("\N{heavy check mark} Deleted ~/.cache/nvim folder...")
     except Exception as e:
         print(e)
 
     system("git clone https://github.com/Mathiew82/lazyvim-config.git ~/.config/nvim")
-    print(" Uninstall and installation was successful!")
+    print("\N{heavy check mark} Uninstall and installation was successful!")
 
 #
 #
